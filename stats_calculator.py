656ef5e6e91dbc52cea53df73955d290f93780ed
Keith Zhang <keithzhang021@outlook.com>
2021-01-14 20:04:50 +0800

    updated stats_calculator

--- stats_calculator.py
+++ stats_calculator.py
@@ -1,6 +1,8 @@
 import os
 import json
 
+from util import COLOR_DICT
+
 f_list = os.listdir("data")
 res = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0}
 for i in f_list:
@@ -10,4 +12,14 @@ for i in f_list:
             for x in range(1, 11):
                 res[f"{x}"] += tmp[f"{x}"]
 
-print(res)
+total = 0
+for i in range(1, 10):
+    total += res[str(i)]
+
+s = ""
+for i in range(1, 10):
+    s += f"{COLOR_DICT[i]}:{res[str(i)]} ------ {format(res[str(i)]/total, '.2f')}" + "\n"
+    print(s)
+
+with open("res.txt", 'w', encoding='utf-8') as f:
+    f.write(s)
